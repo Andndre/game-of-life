8170fea30bde43b0813d9c28b6cadc283339c37c
Andndre <agungandre687@gmail.com>
2022-06-12 23:58:33 +0800

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
